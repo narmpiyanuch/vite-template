ba43607925b9bb012585aa47686a3374963eb813
narmpiyanuch <a.piyanuch@hotmail.com>
2023-09-08 15:33:05 +0700

    basic-react-router-v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import './index.css'
 
+const routes = [
+  { path: '/', element: <div>HomePage</div> },
+  { path: '/profile', element: <div>ProfilePage</div> },
+  { path: '/profile/:userId', element: <div>FriendPage</div> },
+  { path: '/feed', element: <div>FeedPage</div> },
+  { path: '*', element: <Navigate to='/' /> },
+];
+
+const router = createBrowserRouter(routes);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>Vite React Template</div>
+  <RouterProvider router={router} />
 );
