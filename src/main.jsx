a4cfe81a9bc49410fd9fd8436624628552dd0363
narmpiyanuch <a.piyanuch@hotmail.com>
2023-09-08 17:24:59 +0700

    Card-mui

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Appbar from './Appbar';
+import Card from './Card'
+
 
 function App() {
   return <div>
-    <Button variant='text'>Button</Button>
+    {/* <Button variant='text'>Button</Button>
     <Button variant='contained' >Button</Button>
     <Button variant='contained' disabled>Button</Button>
     <Button variant='outlined'>Button</Button>
@@ -21,8 +27,32 @@ function App() {
     >
       Button
     </Button>
+    <Container>
+      <Box
+        sx={{
+          width: 800,
+          height: 300,
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      />
+    </Container> */}
 
-  </div>;
+    <Appbar />
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={4}>
+        <Grid item xs={0} md={2}>SideBar</Grid>
+        <Grid item xs={12} md={8} sx={{ padding: '20px' }}>
+          <Card />
+        </Grid>
+        <Grid item xs={0} md={8}>RightBar</Grid>
+      </Grid>
+    </Box>
+
+  </div >;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
