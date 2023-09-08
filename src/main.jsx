b9bad872c81ca585d546f798fed0eca4d56342ed
narmpiyanuch <a.piyanuch@hotmail.com>
2023-09-08 16:26:05 +0700

    finish-react-router-v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
 import './index.css'
-
-const routes = [
-  { path: '/', element: <div>HomePage</div> },
-  { path: '/profile', element: <div>ProfilePage</div> },
-  { path: '/profile/:userId', element: <div>FriendPage</div> },
-  { path: '/feed', element: <div>FeedPage</div> },
-  { path: '*', element: <Navigate to='/' /> },
-];
-
-const router = createBrowserRouter(routes);
+import App from './App';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
