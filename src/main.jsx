bb4fbbc3e434c2fc6ef9b146f19fe492793783da
narmpiyanuch <a.piyanuch@hotmail.com>
2023-09-08 15:20:27 +0700

    outlet-nestedRoute

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, Navigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import axios from 'axios';
 import './index.css';
 
@@ -53,26 +55,82 @@ function ProfilePage() {
 };
 
 function FriendPage() {
-  return <div className='App'>Friend Page</div>;
+  const { userId } = useParams();
+  const [friend, setFriend] = useState(null);
+
+  const fetchFriendDetail = async () => {
+    try {
+      const { data } = await axios.get(`/users/${userId}`);
+      console.log(data);
+      setFriend(data);
+    } catch (err) {
+      console.log(err)
+    }
+  };
+  useEffect(() => {
+    fetchFriendDetail();
+  }, [])
+  return <div className='App'>
+    {friend && (<div className='friend'>
+      <h3>{friend.name}</h3>
+      <h4>{friend.phone}</h4>
+    </div>)}
+  </div>;
 };
 
 function FeedPage() {
-  return <div className='App'>Feed Page</div>;
+  return <div className='App'>
+
+  </div>;
 };
 
+function NotFoundPage() {
+  return <div className='App'>404: Not Found</div>;
+}
+
+function AppLayout() {
+
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/profile/5">Friend</Link>
+        <Link to="/feed">Feed</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/profile/5">Friend</Link>
-      <Link to="/feed">Feed</Link>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/profile/:id' element={<FriendPage />} />
-        <Route path='/feed' element={<FeedPage />} />
+        {/* Parent */}
+        <Route path='/' element={<AppLayout />} >
+          <Route path='' element={<HomePage />} />
+          <Route path='profile' element={<ProfilePage />} />
+          <Route path='profile/:userId' element={<FriendPage />} />
+          <Route path='feed' element={<FeedPage />} />
+          <Route path='*' element={<Navigate to='/' />} />
+          {/* Child */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
+
+
+
+//<Routes>
+//<Route path='/' element={<HomePage />} />
+//<Route path='/profile' element={<ProfilePage />} />
+//<Route path='/profile/:userId' element={<FriendPage />} />
+//<Route path='/feed' element={<FeedPage />} />
+//<Route path='*' element={<Navigate to='/' />} />
+{/* path ='*' คือ path ใดๆ ที่ไม่ได้ประกาศไว้ข้างบน
+  Navigate เอาไว้สำหรับ user พิมหลัง / ที่มั่วๆ ก็จะให้ Link ไปหน้าที่เรา set ไว้แทน */}
+{/* <Route path='*' element={<NotFoundPage />} /> */ }
+//</Routes>
