f50149cd1b3a88881ea171dca4f65eca0fc7ea00
narmpiyanuch <a.piyanuch@hotmail.com>
2023-09-08 11:17:13 +0700

    contextAuth-test

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React, { createContext, useContext, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+// AuthContext
+const AuthContext = createContext();
+function AuthContextProvider(props) {
+  const [isAuth, setIsAuth] = useState(true);
+  const handleAuth = () => setIsAuth(!isAuth);
+  const shareObj = { handleAuth, isAuth }
+
+  return <AuthContext.Provider value={shareObj}>
+    {props.children}
+  </AuthContext.Provider>
+}
+
+function App() {
+  const { handleAuth, isAuth } = useContext(AuthContext);
+  // const [isAuth, setIsAuth] = useState(true);
+
+  // const handleAuth = () => setIsAuth(!isAuth);
+
+  return (
+    <div className='App'>
+      <h1>Welcome.. {!isAuth ? 'Guest' : 'User'}</h1>
+      <button onClick={handleAuth}>{!isAuth ? 'Login' : 'Logout'}</button>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div>Vite React Template</div>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+
 );
